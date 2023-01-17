ba16aee83e99b1dbb29b8540a0b54ecc712a470b
altsoph <altsoph@gmail.com>
2023-01-17 17:19:56 +0100

    Basic stats check

--- generate-graphs.py
+++ generate-graphs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import igraph as ig
+import networkx as nx
 
 # Set parameters
 n = 50
@@ -30,3 +31,24 @@ def generate_PPM(n=n,p_in=p_in,p_out=p_out,k=2):
 
 def generate_GRG(n=n, r=r):
     return ig.Graph.GRG(n, r, torus=True)
+
+def generate_GRG_square(n=n, r=r):
+    return ig.Graph.GRG(n, r, torus=False)
+
+def ig2edges(g):
+    return [(e.source, e.target) for e in g.es]
+
+def edges2nx(e):
+    return nx.from_edgelist(e)
+
+def ig2nx(g):
+    return edges2nx(ig2edges(g))
+
+def nx2stats(ng):
+    return (
+        nx.average_clustering(ng), # avg_clustering
+        len(ng.edges()), # edges
+        nx.transitivity(ng), # transitivity
+        np.sum(list(nx.triangles(ng).values()))/3, # triangles
+        np.sum([y for x,y in ng.degree()])/len(ng) # avg_degree
+    )
---
+++ generate_10k.py
@@ -0,0 +1,19 @@
+import json
+from generate-graphs import *
+
+
+res = dict()
+res['graphs'] = dict()
+res['stats'] = dict()
+
+for ggn, gg in zip(['ER', 'PPM', 'GRGt', 'GRGs'], [generate_ER, generate_PPM, generate_GRG, generate_GRG_square]):
+    res['graphs'][ggn] = []
+    res['stats'][ggn] = []
+    for _ in range(10000):
+        g = gg()
+        es = ig2edges(g)
+        res['graphs'][ggn].append( es[:] )
+        res['stats'][ggn].append( nx2stats(edges2nx(es)) )
+        
+print(json.dumps(res), file=open('10k_samples.json', 'w'))
+
